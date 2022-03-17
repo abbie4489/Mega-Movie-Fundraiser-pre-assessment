9f9309e4f6dddac72d73718477f9a85ece2aa6ec
abbie4489 <warda@middleton.school.nz>
2022-03-18 11:24:01 +1300

    create 00_MMF_base_v1

---
+++ 00_MMF_base_v1.py
@@ -0,0 +1,24 @@
+# Import statements
+
+# Functions go here
+
+# main routine
+
+# set up dictionaries / list needed to hold data
+
+# Ask user if they have used the program before and
+# show instructions if necessary
+
+# loop to get ticket details
+
+    # get name (cant be blank)
+
+    # Get age (between 12 and 130)
+
+    # calculate snack prices
+
+    # Ask for payment method (an apply surcharge if necessary)
+
+# calculate total sales and profit
+
+# output data to text file
