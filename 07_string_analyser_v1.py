fb1d41c82fa6280a4d2e248b4b3afd7b59bc359f
abbie4489 <warda@middleton.school.nz>
2022-04-04 09:31:51 +1200

    component 7 - 8

---
+++ 07_string_analyser_v1.py
@@ -0,0 +1,10 @@
+"""Trialing methods of extracting and testing the first character in a string
+to see if it a number. in this program all i have done is confirm a method
+of splitting the first character off from the rest of the string
+"""
+
+
+my_string = "hello world"
+
+print(f"First letter: {my_string[0]}")
+print(f"Rest of string: {my_string[1:]}")
